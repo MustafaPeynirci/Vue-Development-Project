232008306eb16754cc5a96facecac3d56b0a7cec
Mustafa Peynirci <mustafapeynircigithub@gmail.com>
2023-01-18 00:28:58 +0300

    alertCount

--- B3/cli-project/src/App.vue
+++ B3/cli-project/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <alertCount></alertCount>
   </div>
 </template>
 
 <script>
+import AlertCount from './components/AlertCount.vue';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    alertCount:AlertCount
   }
 }
 </script>
---
+++ B3/cli-project/src/components/AlertCount.vue
@@ -0,0 +1,17 @@
+<template>
+     <div class="alert alert-success">
+        hello
+        <button @click="count++" class="btn btn-sm btn-primary">{{count}} click</button>    
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            console.log("data girdi");
+            return {
+                count: 0,
+            }
+        }
+    }
+</script>
