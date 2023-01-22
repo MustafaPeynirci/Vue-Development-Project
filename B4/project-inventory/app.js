0ade200af94b5b7b8718ba68a3f873010fea4ffe
Mustafa Peynirci <mustafapeynircigithub@gmail.com>
2023-01-23 01:47:15 +0300

    active - archive added

--- B4/project-inventory/app.js
+++ B4/project-inventory/app.js
@@ -1,16 +1,54 @@
+Vue.component("InventoryList", {
+    template: "#inventory-list-template",
+    props: {
+        title: {type:String, required:true, default:"list"},
+        items: {type:Array, required:true, default:[]}
+    },
+    computed: {
+        totalPrice(){
+            let totalP = 0
+            this.items.forEach(response => {
+                totalP += parseFloat(response.price.toString()) 
+            })
+            return totalP.toFixed(2)
+        },
+        totalUnit(){
+            let totalU = 0
+            this.items.forEach(response => {
+                totalU += parseInt(response.unit.toString())
+            })
+            return totalU
+        }
+    },
+    methods: {
+        changeItemStatus(item){
+            item.archive = !item.archive
+        },
+        deleteItem(id){
+            this.$parent.$data.allItems = this.$parent.$data.allItems.filter(fltr => fltr.id !== id)
+        }
+    }
+})
+
 const App = new Vue({
     el: '#app',
     data: {
         item: {},
         allItems: [
-            {"id": 1, "baslik": "Masa", "kategori": "Mobilya", "adet": "1", "fiyat": 259, "arsiv": false},
-            {"id": 2, "baslik": "Kitaplık", "kategori": "Mobilya", "adet": "2", "fiyat": 159, "arsiv": false},
-            {"id": 3, "baslik": "Monitor", "kategori": "Elektronik", "adet": "5", "fiyat": 400, "arsiv": true}
+            {"id": 1, "title": "Table", "category": "Furniture", "unit": "1", "price": 259, "archive": false},
+            {"id": 2, "title": "Bookself", "category": "Furniture", "unit": "2", "price": 159, "archive": false},
+            {"id": 3, "title": "Monitor", "category": "Electronic", "unit": "5", "price": 400, "archive": true}
         ],
     },
     created() {
     },
     computed: {
+        activeItems(){
+            return this.allItems.filter(fltr => fltr.archive === false)
+        },
+        archiveItems(){
+            return this.allItems.filter(fltr => fltr.archive === true)
+        }
     },
     methods: {
     }
